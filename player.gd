741ad5dedb6f01a5e984e8f6c258f1147ed4e381
mohsin111111111 <mohsinahmad99847@gmail.com>
2026-07-05 16:15:49 +0530

    fixed bugs

--- player.gd
+++ player.gd
@@ -1,6 +1,6 @@
 extends CharacterBody3D
 const WALK_SPEED = 5.0
-const SPRINT_SPEED = 9.0
+const SPRINT_SPEED = 12.0
 const JUMP_VELOCITY = 4.5
 
 var current_speed=WALK_SPEED
@@ -38,11 +38,11 @@ func _physics_process(delta):
 		velocity.y -= gravity * delta
 	else:
 		jump_count=0
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+	if Input.is_action_just_pressed("ui_accept") and jump_count<max_jumps:
 		velocity.y = JUMP_VELOCITY
 		jump_count+=1
 		
-	if Input.is_action_just_pressed("sprint"):
+	if Input.is_action_pressed("sprint"):
 		current_speed=SPRINT_SPEED
 	else:
 		current_speed=WALK_SPEED
