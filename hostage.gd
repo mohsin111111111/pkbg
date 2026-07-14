757cb0b6f953375b70ee599a87438bbae8f1042b
mohsin111111111 <mohsinahmad99847@gmail.com>
2026-07-15 01:07:26 +0530

    did mostly debugging and made helipad

--- hostage.gd
+++ hostage.gd
@@ -1,7 +1,10 @@
 extends Node3D
+
 var is_rescued = false
+
 func rescue():
 	if not is_rescued:
 		is_rescued = true
-		print("Hostage Rescued! Team secured.")
+		print("HOSTAGE: Thank you! They might have taken him with others long time since we saw him")
+		print("HOSTAGE: There is a supply chopper out back. Take it and go find him!")
 		queue_free()
--- stealth_guard.gd
+++ stealth_guard.gd
@@ -1,26 +1,31 @@
 extends StaticBody3D
+
 @onready var path_follower = get_parent()
 var patrol_speed = 3.0
 var player_in_hearing_zone = false
 var player_node = null
+
 func _physics_process(delta):
-	
 	if path_follower is PathFollow3D:
 		path_follower.progress += patrol_speed * delta
 	if player_in_hearing_zone and player_node != null:
 		if player_node.is_crouching == false:
 			catch_player("The guard heard your footsteps!")
+
 func _on_vision_zone_body_entered(body):
 	if body.name == "Player":
 		catch_player("The guard saw you!")
+
 func _on_hearing_zone_body_entered(body):
 	if body.name == "Player":
 		player_in_hearing_zone = true
 		player_node = body 
+
 func _on_hearing_zone_body_exited(body):
 	if body.name == "Player":
 		player_in_hearing_zone = false
 		player_node = null
+
 func catch_player(reason):
 	print("MISSION FAILED: ", reason)
-	get_tree().call_deferred("change_scene_to_file","res://bunker_level.tscn")
+	get_tree().call_deferred("change_scene_to_file", "res://bunker_level.tscn")
